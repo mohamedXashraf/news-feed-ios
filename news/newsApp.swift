1192594d5bac6d1fef75913e217d367f486852d7
Mohamed Ashraf <mohamed.ashraf.10107@gmail.com>
2022-08-12 05:26:57 +0200

    Initial Commit

---
+++ news/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  news
+//
+//  Created by Mohamed on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
